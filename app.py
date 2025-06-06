bf1a4b352f0d366c03901afc42fcf07f4b8b116b
Bosi Chen <chenbosi.bosi@gmail.com>
2025-06-05 23:27:32 -0400

    start page

---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, url_for, g
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+import psycopg2
+
+
+app = Flask(__name__)
+
+
+# Load environment variables from .env
+load_dotenv()
+
+# Fetch variables
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+
+def get_db():
+  # Opens a new database connection if there is none yet for the current application context.
+  if 'db' not in g:
+      g.db = psycopg2.connect(
+          user=USER,
+          password=PASSWORD,
+          host=HOST,
+          port=PORT,
+          dbname=DBNAME
+      )
+  return g.db
+
+@app.teardown_appcontext
+def close_db(e=None):
+    # Closes the database again at the end of the request.
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+
+@app.route('/')
+def index():
+  conn = get_db()
+  cursor = conn.cursor()
+  cursor.execute(f"""
+    select
+      *
+    from
+      single_album
+      join series_test on single_album.series_id = series_test.id
+    order by
+      release_date desc
+  """)
+  data = cursor.fetchall()
+  cursor.close()
+  return render_template('index.html', data=data)
+
+@app.route('/765as')
+def imas():
+   return render_template('imas.html')
+
+@app.route('/cg')
+def cg():
+  return render_template('cg.html')
+
+@app.route('/ml')
+def ml():
+  return render_template('ml.html')
+
+@app.route('/m')
+def m():
+  return render_template('m.html')
+
+@app.route('/sc')
+def sc():
+  return render_template('sc.html')
+
+@app.route('/gkms')
+def gkms():
+  return render_template('gkms.html')
+
+if __name__ == '__main__':
+  app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,113 @@
+// global
+const brandName = {
+  "as": "THE IDOLM@STER",
+  "cg": "Cinderella Girls",
+  "ml": "Million Live",
+  "m": "Side M",
+  "sc": "Shiny Colors",
+  "gkms": "Gakuen IDOLM@STER",
+  "others": "Others"
+}
+
+const brandIconsSrc = {
+  "as": "/static/images/765as.svg",
+  "cg": "/static/images/cg.svg",
+  "ml": "/static/images/ml.svg",
+  "m": "/static/images/m.svg",
+  "sc": "/static/images/sc.svg",
+  "gkms": "/static/images/gkms.svg",
+  "others": "/static/images/others.svg"
+};
+
+// get brand icon
+const getBrandIconSrc = (brand) => {
+  return brandIconsSrc[brand];
+};
+
+//container
+const container = document.getElementById('latest-releases');
+
+const createCover = (cover, title) => {
+  const articleImg = document.createElement('div');
+  articleImg.classList.add('article-img');
+  
+  const coverImg = document.createElement('img');
+  coverImg.src = cover;
+  coverImg.alt = title;
+  coverImg.style = "width: 200px;";
+  
+  articleImg.append(coverImg);
+  return articleImg
+}
+
+const createListItem = (content) => {
+  const item = document.createElement('li');
+  item.textContent = content;
+  return item;
+}
+
+const createTrackList = (tracksArr) => {
+  const list = document.createElement('ol');
+  tracksArr.forEach(t => list.append(createListItem(t)));
+  return list;
+}
+
+const createInfoCard = (title, tracksArr) => {
+  const infoCard = document.createElement('div');
+  infoCard.classList.add('article-info');
+  
+  const releaseTitle = document.createElement('h3');
+  releaseTitle.textContent = title;
+  infoCard.append(releaseTitle);
+
+  const trackList = createTrackList(tracksArr);
+  infoCard.append(trackList);
+  
+  return infoCard;
+}
+
+const createBrandIcon = (brand) => {
+  const articleBrand = document.createElement('div');
+  articleBrand.classList.add('article-brand');
+  
+  const icon = document.createElement('img');
+  icon.src = getBrandIconSrc(brand);
+  icon.alt = `${brandName[brand]} icon`;
+  icon.style = "width: 50px;";
+  
+  articleBrand.append(icon);
+  return articleBrand
+}
+
+const createArticle = (data) => {
+  // data was in object form
+  // const brand = data["series_test"]["brand"];
+  // const cover = data["cover_img"];
+  // const title = data["title"];
+  // const tracksArr = data["track_list"];
+
+  // but now in array...
+  const brand = data[11];
+  const cover = data[3];
+  const title = data[1];
+  const tracksArr = data[8];
+
+  const article = document.createElement('article');
+  article.classList.add(`${brand}-shadow`);
+
+  article.append(createCover(cover, title));
+  article.append(createInfoCard(title, tracksArr));
+  article.append(createBrandIcon(brand));
+
+  return article;
+}
+
+const createReleaseCard = (brand, title) => {
+  const card = document.createElement('div');
+  releaseTitle.textContent = title;
+  card.append(createIcon(brand));
+  card.append(title);
+}
+
+console.log(appData) // for debugging
+appData.forEach(e => container.append(createArticle(e))); 
